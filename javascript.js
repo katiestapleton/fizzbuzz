8154bb7ab8388fc2ce5bff3ec56953b29eb8303b
Katie Stapleton <deepdarkstar7@gmail.com>
2020-12-08 10:39:03 -0600

    basic function parameters

---
+++ javascript.js
@@ -0,0 +1,19 @@
+for (i = 0, i < 100, i++) {
+    n = i+1;
+    // turn joint multiples of 3 and 5 into FizzBuzz
+    if (n % 3 == 0 && n % 5 == 0) {
+        
+    }
+    // turn multiples of 3 into "Fizz"
+    else if (n % 3 == 0) {
+
+    }
+    // turn multiples of 5 into "Buzz"
+    else if (n % 5 == 0) {
+
+    }
+    // print remaining numbers as normal numbers
+    else {
+
+    }
+}
